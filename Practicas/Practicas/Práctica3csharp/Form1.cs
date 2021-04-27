4e1044fd541fe788fa3f3d970960fd514e5f8cc5
JonatanYah <7047@itescam.edu.mx>
2021-04-27 14:20:23 -0500

    Practica3

---
+++ Practicas/Practicas/Práctica3csharp/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Práctica3csharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            String host = "https//itescam.edu.mx";
+            Socket socket = new Socket(host, 100);
+            String ip = socket.getInetAddress().getHostAddress();
+           
+        }
+    }
+}
