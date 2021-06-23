7f6ad08086c4998b65a599bba19637172401cb73
JonatanYah <7047@itescam.edu.mx>
2021-06-22 22:02:07 -0500

    Práctica4

---
+++ Practicas/Java/Practica4/ControlComunicacion/BarcoP.java
@@ -0,0 +1,58 @@
+package ControlComunicacion;
+
+import java.awt.*;
+import javax.swing.ImageIcon;
+
+public class BarcoP {
+	private Image barcovivo;
+	private boolean destruido;
+	boolean jugador;
+
+	/*
+	 *Constructor que tiene un booleano para determinar qué jugador es la pieza del barco.
+* pertenece a. falso es el jugador 2, verdadero es el jugador 1
+	 */
+	public BarcoP(boolean jugador) {
+		this.jugador = jugador;
+	
+	
+			barcovivo = new ImageIcon("Player1.png").getImage();
+		
+	}
+
+	/*
+	 * establece la imagen según el nombre del archivo
+	 */
+	public void setImagen(String file) {
+		barcovivo = new ImageIcon(file).getImage();
+
+	}
+
+	/*
+	 * devuelve la imagen de la pieza del barco
+	 */
+	public Image getImagen() {
+		return barcovivo;
+	}
+
+	/*
+	 * Destruye la pieza del barco estableciendo shipIsDead en verdadero y cambiando el
+*
+	 */
+	public void destruir() {
+		destruido = true;
+		if (jugador) {
+			setImagen("Player1Hit.png");
+		} else {
+			setImagen("Player2Hit.png");
+		}
+	}
+
+	/*
+	 * Retorna la pieza destruida
+	 */
+	public boolean Destruido() {
+		return destruido;
+	}
+
+}
---
+++ Practicas/Java/Practica4/Interfaz/Main.java
@@ -0,0 +1,11 @@
+package Interfaz;
+
+
+import Logica.Logica;
+
+
+public class Main{
+	public static void main(String[] args) {
+		new Logica().setUpWindow();
+	}
+}
